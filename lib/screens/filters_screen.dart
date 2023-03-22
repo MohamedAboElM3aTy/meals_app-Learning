48c62296ce9e2a6a83208e372eff0e88eef38293
Mohamed abo el maaty zaky <98196426+MohamedAboElM3aTy@users.noreply.github.com>
2023-03-22 15:38:16 +0200

    Working on filters and add switches

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -2,19 +2,100 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FilterScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
   static const routeName = '/filtered-screens';
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
+
+  Widget buildSwitch(
+      String title, String description, bool currentValue, Function newUpdate) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(description),
+      onChanged: newUpdate,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Next Page'),
+        title: Text('Filters'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text(
-          'Filtered Screens',
-        ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                buildSwitch(
+                  'Gluten Free',
+                  'Only include Gluten free meals',
+                  _glutenFree,
+                  (newValue) {
+                    setState(
+                      () {
+                        _glutenFree = newValue;
+                      },
+                    );
+                  },
+                ),
+                buildSwitch(
+                  'Lactose Free',
+                  'Only include Lactose free meals',
+                  _lactoseFree,
+                  (newValue) {
+                    setState(
+                      () {
+                        _lactoseFree = newValue;
+                      },
+                    );
+                  },
+                ),
+                buildSwitch(
+                  'Vegan Free',
+                  'Only include Vegan free meals',
+                  _vegan,
+                  (newValue) {
+                    setState(
+                      () {
+                        _vegan = newValue;
+                      },
+                    );
+                  },
+                ),
+                buildSwitch(
+                  'Vegetarian Free',
+                  'Only include Vegetarian free meals',
+                  _vegetarian,
+                  (newValue) {
+                    setState(
+                      () {
+                        _vegetarian = newValue;
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
